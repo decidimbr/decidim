3595cc542e6e9e1f187222dc57c4c8c9bb7820c8
Lucas Pirola <lucaspirola@gmail.com>
2026-09-01 20:50:15 -0300

    fix(extra-blocks): register assets via Decidim::Shakapacker on 0.32
    Decidim::Webpacker.register_path/register_entrypoints are deprecated in
    decidim 0.32; prefer Decidim::Shakapacker with a fallback for older
    versions.

--- vendor/modules/extra-blocks/config/assets.rb
+++ vendor/modules/extra-blocks/config/assets.rb
@@ -2,7 +2,10 @@
 
 base_path = File.expand_path("..", __dir__)
 
-Decidim::Webpacker.register_path("#{base_path}/app/packs")
-Decidim::Webpacker.register_entrypoints(
+# Decidim::Webpacker was renamed to Decidim::Shakapacker in Decidim 0.32
+packer = defined?(Decidim::Shakapacker) ? Decidim::Shakapacker : Decidim::Webpacker
+
+packer.register_path("#{base_path}/app/packs")
+packer.register_entrypoints(
   decidim_extra_blocks: "#{base_path}/app/packs/entrypoints/decidim_extra_blocks.js"
 )
